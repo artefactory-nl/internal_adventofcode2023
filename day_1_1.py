2e6a4b5d2e71ab2b82597f42f81f7d2bfe320a3a
ArthurLambert06 <arthur.lambert@artefact.com>
2023-12-04 17:14:28 +0100

    Add day 1 solution

---
+++ day_1_1.py
@@ -0,0 +1,36 @@
+def is_int(char: str) -> bool: 
+    try:
+        int(char)
+        return True
+    except ValueError:
+        return False
+    
+def read_input(path: str) -> list[str]:
+    with open(path, 'r') as file:
+        return [line.replace("\n", "") for line in file.readlines()]
+
+def solve(lines: list[str]) -> int:
+    total_sum = 0
+    for line in lines:
+        line_length = len(line)
+        first_discovered = False
+        last_discovered = False
+        for index in range(line_length):
+            left_char = line[index]
+            right_char = line[line_length - index - 1]
+            if not first_discovered:
+                if is_int(left_char):
+                    first_number = int(left_char)
+                    first_discovered = True
+            if not last_discovered:
+                if is_int(right_char):
+                    last_number = int(right_char)
+                    last_discovered = True
+            if first_discovered and last_discovered:
+                total_sum += int(str(first_number) + str(last_number))
+                break
+    return total_sum
+
+if __name__ == '__main__':
+    lines = read_input('input_day_1-1.txt')
+    print(solve(lines))
---
+++ day_1_2.py
@@ -0,0 +1,39 @@
+import regex as re
+
+
+from day_1_1 import read_input, solve
+
+
+DIGIT_MAPPING = {
+    "one": "1",
+    "two": "2",
+    "three": "3",
+    "four": "4",
+    "five": "5",
+    "six": "6",
+    "seven": "7",
+    "eight": "8",
+    "nine": "9"
+}
+
+
+def apply_digit_mapping(lines: list[str], mapping: dict[str, str]) -> list[str]:
+    for line_index in range(len(lines)):
+        line = lines[line_index]
+        matches = [match for match in re.finditer(f"({'|'.join(mapping.keys())})", line, overlapped=True)]
+        if matches:
+            if len(matches) == 1:
+                line = line.replace(matches[0].group(), mapping[matches[0].group()])
+            else:
+                first_match = matches[0]
+                last_match = matches[-1]
+                line = line[:first_match.start()] + mapping[first_match.group()] + line[first_match.start():]
+                line = line[:last_match.end() + 1] + mapping[last_match.group()] + line[last_match.end() + 1:]
+        lines[line_index] = line
+    return lines
+
+
+if __name__ == "__main__":
+    lines = read_input('input_day_1-1.txt')
+    lines = apply_digit_mapping(lines, DIGIT_MAPPING)
+    print(solve(lines))
