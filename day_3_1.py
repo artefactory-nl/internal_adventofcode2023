8cb9952ef44252440f04d2d1147c05711e8db8d2
ArthurLambert06 <arthur.lambert@artefact.com>
2023-12-05 17:07:35 +0100

    Add day 3

---
+++ day_3_1.py
@@ -0,0 +1,65 @@
+import re
+
+from utils import read_input
+
+
+def extract_symbols(lines: list[str]) -> list[str]:
+    symbols = []
+    for line in lines:
+        for char in line:
+            if char not in symbols and not char.isdigit() and char != ".":
+                symbols.append(char)
+    return symbols
+
+def find_numbers_positions(lines: list[str]) -> list[dict[str, list[tuple[int, int]]]]:
+    """Returns a list of dictionaries, each dictionary contains the positions of a number in the grid."""
+    positions = []
+    for line_index, line in enumerate(lines):
+        digits_in_line = re.finditer(r"\d+", line)
+        for match in digits_in_line:
+            positions.append({str(match.group()): [(line_index, i) for i in range(match.start(), match.end())]})
+    return positions
+
+def extract_possible_coordinates(coordinates: tuple[int, int], max_x: int, max_y: int) -> list[tuple[int, int]]:
+    """Returns a list of coordinates that are around the given coordinates."""
+    x, y = coordinates
+    possible_coordinates = []
+    for i in [-1, 0, 1]:
+        for j in [-1, 0, 1]:
+            if (i, j) != (0, 0):
+                if x + i >= 0 and y + j >= 0 and x + i < max_x and y + j < max_y:
+                    possible_coordinates.append((x + i, y + j))
+    return possible_coordinates
+
+def extract_around_positions(positions: list[dict[str, list[tuple[int, int]]]], max_x: int, max_y: int) -> list[dict[str, list[tuple[int, int]]]]:
+    """Returns a list of dictionaries, each dictionary contains the positions of a number in the grid."""
+    positions_to_check = []
+    for position in positions:
+        for number, positions_list in position.items():
+            around_positions = {
+                number: []
+            }
+            for coordinates in positions_list:
+                possible_around_coordinates = extract_possible_coordinates(coordinates, max_x=max_x, max_y=max_y)
+                possible_around_coordinates = [coord for coord in possible_around_coordinates if coord not in positions_list]
+                around_positions[number].extend(possible_around_coordinates)
+            positions_to_check.append(around_positions)
+    return positions_to_check
+
+def solve(lines: list[str]) -> int:
+    total_sum = 0
+    symbols = extract_symbols(lines)
+    positions = find_numbers_positions(lines)
+    around_positions = extract_around_positions(positions, max_x=len(lines), max_y=len(lines[0]))
+    for around_position in around_positions:
+        for number, coords_to_check in around_position.items():
+            for coordinates in coords_to_check:
+                x, y = coordinates
+                if lines[x][y] in symbols:
+                    total_sum += int(number)
+                    break
+    return total_sum
+
+if __name__ == '__main__':
+    lines = read_input('input_day_3.txt')
+    print(solve(lines))
---
+++ day_3_2.py
@@ -0,0 +1,34 @@
+from day_3_1 import find_numbers_positions, extract_around_positions, extract_possible_coordinates
+from utils import read_input
+
+
+SYMBOLS = ["*"]
+
+
+def extract_adjacent_numbers_to_symbols(lines: list[str], around_positions: list[dict[str, list[tuple[int, int]]]]) -> dict[str, list[int]]:
+    """Returns a dictionary containing the numbers adjacent to a symbol."""
+    numbers_adjacent_to_symbols = {}
+    for around_position in around_positions:
+        for number, coords_to_check in around_position.items():
+            for coordinates in coords_to_check:
+                x, y = coordinates
+                if lines[x][y] in SYMBOLS:
+                    if str(coordinates) not in numbers_adjacent_to_symbols:
+                        numbers_adjacent_to_symbols[str(coordinates)] = []
+                    numbers_adjacent_to_symbols[str(coordinates)].append(int(number))
+                    break
+    return numbers_adjacent_to_symbols
+
+def solve(lines: list[str]) -> int:
+    total_sum = 0
+    positions = find_numbers_positions(lines)
+    around_positions = extract_around_positions(positions, max_x=len(lines), max_y=len(lines[0]))
+    numbers_adjacent_to_symbols = extract_adjacent_numbers_to_symbols(lines, around_positions)
+    for symbols_coords, adj_numbers_list in numbers_adjacent_to_symbols.items():
+        if len(adj_numbers_list) == 2:
+            total_sum += adj_numbers_list[0] * adj_numbers_list[1]
+    return total_sum
+
+if __name__ == '__main__':
+    lines = read_input('input_day_3.txt')
+    print(solve(lines))
